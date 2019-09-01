b5987867caab13fff168443a0c8e56c5c538aaba
Ashleigh <ashleighc207@gmail.com>
2019-09-01 15:23:53 -0400

    adding in some logic for coin flip

--- src/Coin.js
+++ src/Coin.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import './Coin.css';
 
 class Coin extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dogCount: 0,
+      catCount: 0,
+      icons: ['fa-cat', 'fa-dog'],
+      total: 0
+    }
+  }
+  flipCoin() {
+    let randIdx = Math.floor(Math.random() * this.state.icons.length);
+  }
   render(){
     return(
       <section>
