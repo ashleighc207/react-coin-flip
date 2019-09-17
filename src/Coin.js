ddaa107c540e3b367847d3f905069fc4e3598ab8
Ashleigh Christopher <achristopher@MGL148.local>
2019-09-17 16:28:09 -0400

    get state functioning on count of clicks

--- src/Coin.js
+++ src/Coin.js
@@ -4,24 +4,12 @@ import './Coin.css';
 class Coin extends Component {
   constructor(props){
     super(props);
-    this.flipCoin = this.flipCoin.bind(this);
-    this.state = {
-      icon: "fas fa-dog"
-    }
   }
-
-  flipCoin() {
-    let randIdx = Math.floor(Math.random() * this.props.faces);
-    this.setState({
-      icon: randIdx === 0 ? "fas fa-dog" : "fas fa-cat"
-    })
-  }
-
   render(){
     return(
-      <section onClick={this.flipCoin}>
+      <section>
         <div className="Coin">
-          <i className={this.state.icon + " Coin--icon"}></i>
+          <i className={this.props.icon + " Coin--icon"}></i>
         </div>
       </section>
     )
--- src/Gameboard.js
+++ src/Gameboard.js
@@ -4,21 +4,52 @@ import Coin from './Coin';
 
 
 class Gameboard extends Component {
+  static defaultProps = {
+    icons: ['fa-cat', 'fa-dog']
+  }
   constructor(props){
     super(props);
     this.state = {
       dogCount: 0,
       catCount: 0,
-      icons: ['fa-cat', 'fa-dog'],
-      total: 0
+      total: 0,
+      icon: 'fas fa-dog'
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  flipCoin() {
+    let randIdx = Math.floor(Math.random() * this.props.icons.length);
+
+    this.setState(state => {
+      let newState = {
+        icon: state.icon,
+        total: state.total + 1,
+        dogCount: state.dogCount,
+        catCount: state.catCount
+      }
+      if(randIdx === 0){
+        newState.icon = "fas fa-dog";
+        newState.dogCount += 1;
+      } else {
+        newState.icon = "fas fa-cat";
+        newState.catCount += 1;
+      }
+      return newState;
+    })
+  }
+
+  handleClick(e) {
+    this.flipCoin();
   }
 
   render(){
     return(
       <section className="Gameboard">
-        <h1> My Test </h1>
-        <Coin faces={this.state.icons.length}/>
+        <h1>Flip it</h1>
+        <Coin icon={this.state.icon}/>
+        <button onClick={this.handleClick}>Flip me!</button>
+        <p>Out of {this.state.total} flips, {this.state.dogCount} were dog and {this.state.catCount} were cat.</p>
       </section>
     )
   }
