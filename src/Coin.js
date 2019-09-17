474450d0c2751eaf22e8075264383ae4c0c851be
Ashleigh Christopher <achristopher@MGL148.local>
2019-09-17 16:30:44 -0400

    remove constructor for stateless component

--- src/Coin.js
+++ src/Coin.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './Coin.css';
 
 class Coin extends Component {
-  constructor(props){
-    super(props);
-  }
   render(){
     return(
       <section>
