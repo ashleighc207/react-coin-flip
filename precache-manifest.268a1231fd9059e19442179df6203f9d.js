b0f77c3af5662ca64a81e094b8ed013e9c770fa8
Ashleigh Christopher <achristopher@mindgrub.com>
2019-09-24 15:16:05 -0400

    Updates

---
+++ precache-manifest.268a1231fd9059e19442179df6203f9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba83cbcbd390d6370a2c04b3aff9f66",
+    "url": "/react-coin-flip/index.html"
+  },
+  {
+    "revision": "5669d2e1d178342c882e",
+    "url": "/react-coin-flip/static/css/main.0e3802a7.chunk.css"
+  },
+  {
+    "revision": "a52fafe6e3942fe19435",
+    "url": "/react-coin-flip/static/js/2.cd0b081f.chunk.js"
+  },
+  {
+    "revision": "5669d2e1d178342c882e",
+    "url": "/react-coin-flip/static/js/main.d7f0d0f7.chunk.js"
+  },
+  {
+    "revision": "3982da8ef8813e81ff88",
+    "url": "/react-coin-flip/static/js/runtime~main.412838bd.js"
+  }
+]);
